86da7c9e3195d311b1b2c66a6c13f9d6d7d342bf
Wizard Lee <282539259@qq.com>
2016-01-05 16:27:22 +0800

    minor change

---
+++ DemoDynamicCollectionView/EntryTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DemoDynamicCollectionView
+//
+//  Created by Wizard Li on 1/5/16.
+//  Copyright © 2016 morgenworks. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
