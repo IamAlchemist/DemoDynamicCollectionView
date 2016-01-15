984f77c908b384bded56003c48e487094129966a
Wizard Lee <282539259@qq.com>
2016-01-15 14:05:22 +0800

    minor change

--- DemoDynamicCollectionView/EntryDataSource.swift
+++ DemoDynamicCollectionView/EntryDataSource.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class EntryDataSource: NSObject, UITableViewDataSource {
     
-    var data = ["Springy", "Newtownian", "SimpleDynamic", "Calendar", "Pinterest", "SnapDynamic"]
+    var data = ["Springy", "Newtownian", "SimpleDynamic", "Calendar", "Pinterest", "SnapDynamic", "BounceMenu"]
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
