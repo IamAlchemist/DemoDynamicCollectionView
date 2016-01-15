54a183ef99b0e79f8b8fde147ff6db6d40d99c4a
Wizard Lee <282539259@qq.com>
2016-01-15 14:38:37 +0800

    minor change

---
+++ DemoDynamicCollectionView/BounceMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BounceMenuViewController.swift
+//  DemoDynamicCollectionView
+//
+//  Created by Wizard Li on 1/15/16.
+//  Copyright © 2016 morgenworks. All rights reserved.
+//
+
+import UIKit
+
+class BounceMenuViewController : UIViewController {
+    @IBOutlet weak var contentView: UIView!
+}
