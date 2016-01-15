ca2cbceb3cad6cd55e13e5feef861af46b4ca8e1
IamAlchemist <wizard.lee@qq.com>
2016-01-15 17:01:35 +0800

    minor change

--- DemoDynamicCollectionView/BounceMenuViewController.swift
+++ DemoDynamicCollectionView/BounceMenuViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class BounceMenuViewController : UIViewController {
+class BounceMenuViewController : UIViewController{
     
     @IBOutlet weak var contentView: UIView!
     
     var dynamicAnimator : UIDynamicAnimator!
+    var pushBehavior : UIPushBehavior?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,20 +25,43 @@ class BounceMenuViewController : UIViewController {
         contentView.layer.shadowRadius = 5.0
         
         dynamicAnimator = UIDynamicAnimator(referenceView: view)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        dynamicAnimator.removeAllBehaviors()
         
         let collision = UICollisionBehavior(items: [contentView])
         collision.setTranslatesReferenceBoundsIntoBoundaryWithInsets(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -280))
         dynamicAnimator.addBehavior(collision)
-        
+
         let gravity = UIGravityBehavior(items: [contentView])
         gravity.gravityDirection = CGVector(dx: -1, dy: 0)
         dynamicAnimator.addBehavior(gravity)
-        
+
         let push = UIPushBehavior(items: [contentView], mode: .Instantaneous)
         push.magnitude = 0.0
         push.angle = 0.0
         dynamicAnimator.addBehavior(push)
+        pushBehavior = push
+
+        let itemBehavior = UIDynamicItemBehavior(items: [contentView])
+        itemBehavior.elasticity = 0.7
+        itemBehavior.allowsRotation = false
+        dynamicAnimator.addBehavior(itemBehavior)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        super.prepareForSegue(segue, sender: sender)
         
-        
+        if segue.identifier == "contentViewController" {
+            ((segue.destinationViewController as? UINavigationController)?.topViewController as? BounceContentViewController)?.delegate = self
+        }
+    }
+}
+
+extension BounceMenuViewController : BounceContentDelegate {
+    func bounceButtonTapped() {
+        pushBehavior?.pushDirection = CGVector(dx: 35, dy: 0)
+        pushBehavior?.active = true
     }
 }
