dfd7834e52a17d76ea51a7db395146e4cd57c837
IamAlchemist <wizard.lee@qq.com>
2016-05-13 07:29:12 +0800

    fix crash bug

--- DemoDynamicCollectionView/SnapDynamicviewController.swift
+++ DemoDynamicCollectionView/SnapDynamicviewController.swift
@@ -13,7 +13,29 @@ class SnapDynamicViewController : UIViewController {
     
     var dynamicAnimator : UIDynamicAnimator!
     
-    private var myContext = 0;
+    private var myContext = 0
+    private var alertView : AlertView? {
+        didSet{
+            if let oldValue = oldValue {
+                dynamicAnimator.removeAllBehaviors()
+                
+                oldValue.removeObserver(self, forKeyPath: "center", context: &myContext)
+                
+                oldValue.removeFromSuperview()
+            }
+            
+            if let newValue = alertView {
+                view.addSubview(newValue)
+                let snapBehavior = UISnapBehavior(item: newValue, snapToPoint: view.center)
+                dynamicAnimator.addBehavior(snapBehavior)
+                
+                let tapGesture = UITapGestureRecognizer(target: self, action: #selector(alertViewTapped(_:)))
+                newValue.addGestureRecognizer(tapGesture)
+                
+                newValue.addObserver(self, forKeyPath: "center", options: .New, context: &myContext)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,23 +43,21 @@ class SnapDynamicViewController : UIViewController {
         dynamicAnimator = UIDynamicAnimator(referenceView: view)
     }
     
-    @IBAction func showAlert(sender: UIButton) {
-        let alertView = createAlertView()
-        view.addSubview(alertView)
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated);
         
-        let snapBehavior = UISnapBehavior(item: alertView, snapToPoint: view.center)
-        dynamicAnimator.addBehavior(snapBehavior)
+        alertView = nil;
     }
     
-    func createAlertView() -> UIView {
+    @IBAction func showAlert(sender: UIButton) {
+        self.alertView = createAlertView()
+    }
+    
+    func createAlertView() -> AlertView {
         let frame = CGRect(x: self.view.center.x - 50, y: -100, width: 150, height: 100)
         let view = AlertView(frame: frame)
         view.backgroundColor = UIColor.orangeColor()
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: "alertViewTapped:")
-        view.addGestureRecognizer(tapGesture)
-        
-        view.addObserver(self, forKeyPath: "center", options: .New, context: &myContext)
         return view
     }
     
@@ -62,12 +82,7 @@ class SnapDynamicViewController : UIViewController {
             let bottomOfContainer = CGRectGetMaxY(view.bounds)
             if minY > bottomOfContainer && !alertView.isRemoving {
                 alertView.isRemoving = true
-                if let gravity = alertView.gravity {
-                    dynamicAnimator.removeBehavior(gravity)
-                }
-                
-                alertView.removeObserver(self, forKeyPath: "center")
-                alertView.removeFromSuperview()
+                self.alertView = nil
             }
         }
     }
