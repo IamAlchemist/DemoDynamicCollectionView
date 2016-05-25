45cf4ee4ae2e2bb906d76c8929e41385d1a6e87c
Wizard Lee <282539259@qq.com>
2016-05-25 17:33:30 +0800

    minor change

--- DemoDynamicCollectionView/EntryDataSource.swift
+++ DemoDynamicCollectionView/EntryDataSource.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class EntryDataSource: NSObject, UITableViewDataSource {
     
-    var data = ["Springy", "Newtownian", "SimpleDynamic", "Calendar", "Pinterest", "SnapDynamic", "BounceMenu", "CollectionTransition"];
+    var data = ["Springy", "Newtownian", "SimpleDynamic", "Calendar", "Pinterest", "SnapDynamic", "BounceMenu", "CollectionTransition", "CollectionAnimations"];
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
